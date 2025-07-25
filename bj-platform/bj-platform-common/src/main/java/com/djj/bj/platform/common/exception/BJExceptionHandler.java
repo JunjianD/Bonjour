89250549a8bc89f7b6927fbd62ad0d19a37f026e
JunjianD <pkngra@163.com>
2025-07-25 10:44:11 +0800

    feat: 添加日志记录功能以增强异常处理 / Add logging functionality to enhance exception handling

--- bj-platform/bj-platform-common/src/main/java/com/djj/bj/platform/common/exception/BJExceptionHandler.java
+++ bj-platform/bj-platform-common/src/main/java/com/djj/bj/platform/common/exception/BJExceptionHandler.java
@@ -3,6 +3,8 @@ package com.djj.bj.platform.common.exception;
 import com.djj.bj.platform.common.model.enums.HttpCode;
 import com.djj.bj.platform.common.response.ResponseMessage;
 import com.djj.bj.platform.common.response.ResponseMessageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @ControllerAdvice
 public class BJExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(BJExceptionHandler.class);
+
     @ResponseBody
     @ExceptionHandler(value = BJException.class)
     public ResponseMessage<String> handleBJException(BJException e) {
@@ -28,6 +32,7 @@ public class BJExceptionHandler {
     @ResponseBody
     @ExceptionHandler(value = Exception.class)
     public ResponseMessage<String> handleException(Exception e) {
+        logger.error(e.getMessage());
         return ResponseMessageFactory.getErrorResponseMessage(HttpCode.PROGRAM_ERROR);
     }
 }
