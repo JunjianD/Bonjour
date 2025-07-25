b5f1b405072538668c5ff36c0979d54a3e8b6d99
JunjianD <pkngra@163.com>
2025-07-25 10:22:29 +0800

    feat: sdk添加用户在线状态检查和在线用户列表功能 / sdk Add user online status check and online user list functionality

--- bj-sdk/bj-sdk-core/src/main/java/com/djj/bj/sdk/core/client/Client.java
+++ bj-sdk/bj-sdk-core/src/main/java/com/djj/bj/sdk/core/client/Client.java
@@ -1,7 +1,11 @@
 package com.djj.bj.sdk.core.client;
 
+import com.djj.bj.common.io.enums.TerminalType;
 import com.djj.bj.common.io.model.GroupChat;
 import com.djj.bj.common.io.model.PrivateChat;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 客户端
@@ -29,4 +33,28 @@ public interface Client {
      * @param <T>       消息类型
      */
     <T> void sendGroupMessage(GroupChat<T> groupChat);
+
+    /**
+     * 检查用户是否在线
+     *
+     * @param userId 用户ID
+     * @return true 在线，false 不在线
+     */
+    Boolean isOnline(Long userId);
+
+    /**
+     * 获取在线用户列表
+     *
+     * @param userIds 用户ID列表
+     * @return 在线用户ID列表
+     */
+    List<Long> getOnlineUserList(List<Long> userIds);
+
+    /**
+     * 获取用户在线的终端列表
+     *
+     * @param userIds 用户ID列表
+     * @return 用户ID与其在线终端类型的映射
+     */
+    Map<Long, List<TerminalType>> getOnlineTerminal(List<Long> userIds);
 }
--- bj-sdk/bj-sdk-core/src/main/java/com/djj/bj/sdk/core/client/impl/DefaultClient.java
+++ bj-sdk/bj-sdk-core/src/main/java/com/djj/bj/sdk/core/client/impl/DefaultClient.java
@@ -1,11 +1,15 @@
 package com.djj.bj.sdk.core.client.impl;
 
+import com.djj.bj.common.io.enums.TerminalType;
 import com.djj.bj.common.io.model.GroupChat;
 import com.djj.bj.common.io.model.PrivateChat;
 import com.djj.bj.sdk.core.client.Client;
 import com.djj.bj.sdk.interfaces.sender.Sender;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 默认客户端实现类
@@ -31,5 +35,20 @@ public class DefaultClient implements Client {
     @Override
     public <T> void sendGroupMessage(GroupChat<T> groupChat) {
         sender.sendGroupMessage(groupChat);
+    }
+
+    @Override
+    public Boolean isOnline(Long userId) {
+        return sender.isOnline(userId);
+    }
+
+    @Override
+    public List<Long> getOnlineUserList(List<Long> userIds) {
+        return sender.getOnlineUserIds(userIds);
+    }
+
+    @Override
+    public Map<Long, List<TerminalType>> getOnlineTerminal(List<Long> userIds) {
+        return sender.getOnlineTerminalMap(userIds);
     }
 }
