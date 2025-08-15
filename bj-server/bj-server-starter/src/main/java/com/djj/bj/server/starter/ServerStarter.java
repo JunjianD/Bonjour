1f48d180ec0ac9a01c70b8c08f5d5c83daa64497
JunjianD <pkngra@163.com>
2025-08-15 18:56:28 +0800

    feat: 添加跨域配置类以支持不同模块的CORS设置 / Add CORS configuration class to support CORS settings for different modules

---
+++ bj-server/bj-server-starter/src/main/java/com/djj/bj/server/starter/ServerStarter.java
@@ -0,0 +1,28 @@
+package com.djj.bj.server.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 后端服务启动类
+ *
+ * @author jj_D
+ * @version 1.0
+ * @projectName Bonjour
+ * @package com.djj.bj.server.starter
+ * @className ServerStarter
+ * @date 2025/8/13 19:56
+ */
+@EnableAsync
+@EnableScheduling
+@ComponentScan(basePackages = {"com.djj.bj"})
+@SpringBootApplication
+public class ServerStarter {
+    public static void main(String[] args) {
+        System.setProperty("nacos.logging.default.config.enabled", "false");
+        SpringApplication.run(ServerStarter.class, args);
+    }
+}
