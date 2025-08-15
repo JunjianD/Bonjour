e7ef5dd1429953073d6af7b7198d8a939dd78250
JunjianD <pkngra@163.com>
2025-08-15 18:56:28 +0800

    feat: 添加跨域配置类以支持不同模块的CORS设置 / Add CORS configuration class to support CORS settings for different modules

---
+++ bj-platform/bj-platform-friend/bj-platform-friend-starter/src/main/java/com/djj/bj/platform/friend/starter/config/CorsConfig.java
@@ -0,0 +1,49 @@
+package com.djj.bj.platform.friend.starter.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * 跨域配置类
+ *
+ * @author jj_D
+ * @version 1.0
+ * @projectName Bonjour
+ * @package com.djj.bj.platform.friend.starter.config
+ * @className CorsConfig
+ * @date 2025/8/12 12:08
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean = new FilterRegistrationBean<>();
+        //添加CORS配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许的域，不要写*，否则cookie就无法使用了
+//        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
+        //允许的头信息
+        corsConfiguration.addAllowedHeader("*");
+        //允许的请求方式
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE"));
+        //是否发送cookie信息
+        corsConfiguration.setAllowCredentials(true);
+        //预检请求的有效期，单位为秒
+        corsConfiguration.setMaxAge(3600L);
+
+        //添加映射路径，标识待拦截的请求
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        corsFilterFilterRegistrationBean.setFilter(new CorsFilter(source));
+        corsFilterFilterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return corsFilterFilterRegistrationBean;
+    }
+}
